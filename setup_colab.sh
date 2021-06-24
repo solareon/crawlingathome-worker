a5ea627772dfe576c6f3c2e02f8babbe31a82b68
ARKseal <56448385+ARKseal@users.noreply.github.com>
2021-06-24 09:10:51 -0500

    get clip_filter in setup_colab

--- setup_colab.sh
+++ setup_colab.sh
@@ -10,6 +10,7 @@ git clone "https://github.com/TheoCoombes/crawlingathome" crawlingathome_client
 wget https://raw.githubusercontent.com/rvencu/crawlingathome-worker/colab-mod-asks/requirements.txt
 wget https://raw.githubusercontent.com/Wikidepia/crawlingathome-worker/master/blocklist-domain.txt
 wget https://raw.githubusercontent.com/ARKseal/crawlingathome-worker/dynamic-chunk/crawlingathome.py
+wget https://raw.githubusercontent.com/ARKseal/crawlingathome-worker/dynamic-chunk/clip_filter.py
 
 pip3 install torch==1.7.1$1 torchvision==0.8.2$1 -f https://download.pytorch.org/whl/torch_stable.html
 
