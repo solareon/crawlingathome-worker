fba14789a6bddd34a9f670c217a0d648a7cf30d8
ARKseal <56448385+ARKseal@users.noreply.github.com>
2021-07-14 09:47:26 -0500

    Update setup_colab.sh

--- setup_colab.sh
+++ setup_colab.sh
@@ -8,9 +8,9 @@ rm -r crawlingathome_client
 
 git clone "https://github.com/TheoCoombes/crawlingathome" crawlingathome_client
 
-wget https://raw.githubusercontent.com/ARKseal/crawlingathome-worker/master/crawlingathome.py
-wget https://raw.githubusercontent.com/ARKseal/crawlingathome-worker/master/requirements.txt
-wget https://raw.githubusercontent.com/ARKseal/crawlingathome-worker/master/clip_filter.py
+wget https://raw.githubusercontent.com/ARKseal/crawlingathome-worker/update-script/crawlingathome.py
+wget https://raw.githubusercontent.com/ARKseal/crawlingathome-worker/update-script/requirements.txt
+wget https://raw.githubusercontent.com/ARKseal/crawlingathome-worker/update-script/clip_filter.py
 
 wget https://raw.githubusercontent.com/rvencu/crawlingathome-gpu-hcloud/main/blocklists/blocklist-domain.txt
 wget https://raw.githubusercontent.com/rvencu/crawlingathome-gpu-hcloud/main/blocklists/failed-domains.txt
